9d9d5e84101acd225d7ca109a5ed02ba1fc438a4
Murat Bas <basmurat@outlook.com.tr>
2026-03-24 16:51:49 +0300

    some animations and sprites are added

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -6,13 +6,25 @@ const JUMP_VELOCITY = -980.0
 
 
 func _physics_process(delta: float) -> void:
+	if velocity.x > 1 or velocity.x < -1:
+		$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("idle")
+
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
